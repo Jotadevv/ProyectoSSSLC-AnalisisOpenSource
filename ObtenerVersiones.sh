9242d21aa4c50ef5869747465c61379f5076635b
JONATAN C <jonatan.carrillo@urosario.edu.co>
2026-04-02 12:53:35 -0500

    Change pip freeze output to JSON file
    Modify script to output dependencies in JSON format.

--- ObtenerVersiones.sh
+++ ObtenerVersiones.sh
@@ -1,1 +1,9 @@
-pip freeze
+pip freeze | python -c "
+import sys, json
+deps = []
+for line in sys.stdin:
+    if '==' in line:
+        name, version = line.strip().split('==')
+        deps.append({'name': name, 'version': version})
+print(json.dumps(deps, indent=2))
+" > dependencias.json
